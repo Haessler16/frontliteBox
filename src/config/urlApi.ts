482ba9c1a8ebd77f12bba7dd35bcebd270042dca
Haessler <lincetvm16@gmail.com>
2025-02-18 00:49:51 -0400

    Switch API endpoint from local to external deployment

--- src/config/urlApi.ts
+++ src/config/urlApi.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-const localAPI = 'http://localhost:4200/'
+// const localAPI = 'http://localhost:4200/'
 const externalAPI = 'https://postlitebox.onrender.com/'
 
-export const api = localAPI
+export const api = externalAPI
